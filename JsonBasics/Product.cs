632e9c2fbacf74aceee81466d0d58916b0787706
sreemonta20 <sbhowmikcse08@gmail.com>
2021-07-06 22:11:16 +0600

    Add project files.

---
+++ JsonBasics/Product.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+internal class Product
+{
+    public string Name { get; set; }
+    public DateTime Expiry { get; set; }
+    public string[] Sizes { get; set; }
+}
---
+++ JsonBasics/Program.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+Console.WriteLine("---------Serialize Object to json-----------------");
+Product product = new Product();
+product.Name = "Apple";
+product.Expiry = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+product.Sizes = new string[] { "Small" };
+
+// When class object is need to be converted into equivalent json string with beautifier
+string json = JsonConvert.SerializeObject(product, Formatting.Indented);
+
+//// When class object is need to be converted into equivalent json string without
+//string json = JsonConvert.SerializeObject(product);
+Console.WriteLine(json);
+
+
+Console.WriteLine("---------Deserialize Object-----------------");
+string jsonstr = @"{
+  'Name': 'Bad Boys',
+  'Expiry': '1995-4-7T00:00:00',
+  'Sizes': [
+    'Large',
+    'Small'
+  ]
+}";
+
+Product m = JsonConvert.DeserializeObject<Product>(jsonstr);
+
+string name = m.Name;
+
+Console.WriteLine($"Name is {m.Name} Expiry date : {m.Expiry.ToShortDateString()},  Sizes are  {m.Sizes[0]} and {m.Sizes[1]}");
+
+
+Console.WriteLine("---------Linq to Json-----------------");
+JArray array = new JArray();
+array.Add("Orange");
+array.Add(new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day));
+array.Add(new string[] { "African", "Asian"});
+
+JObject oJObject = new JObject();
+oJObject["Product"] = array;
+
+string jsonProd = oJObject.ToString();
+Console.WriteLine(jsonProd);
+
+
+
+Console.ReadKey();
